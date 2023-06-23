839f1dd7370a4f3bd34632a29ce50340c47a95d2
pseudokawaii@gmail.com <pseudokawaii@gmail.com>
2023-06-24 01:40:48 +0600

    Update .netrc location

--- bot/__init__.py
+++ bot/__init__.py
@@ -631,7 +631,7 @@ if not ospath.exists('.netrc'):
     with open('.netrc', 'w'):
         pass
 srun(["chmod", "600", ".netrc"])
-srun(["cp", ".netrc", "/root/.netrc"])
+srun(["cp", ".netrc", "${HOME}/.netrc"])
 srun(["chmod", "+x", "aria.sh"])
 srun("./aria.sh", shell=True)
 if ospath.exists('accounts.zip'):
